68fc593a64227610cb9b1289636f3b58be2fb8dd
Mohammad <davoudiofficial@gmail.com>
2025-01-10 21:36:02 +0330

    tada: v2

--- internal/pkg/cache/core.go
+++ internal/pkg/cache/core.go
@@ -1,8 +1,8 @@
 package cache
 
 import (
+	"context"
 	"go.uber.org/zap"
-	"sync"
 	"time"
 )
 
@@ -10,7 +10,6 @@ type Core struct {
 	log                 *zap.SugaredLogger
 	data                map[string]interface{}
 	sortedKeyExpiration []ExpirationData
-	mu                  sync.RWMutex
 
 	SetCh    chan SetCacheReq
 	GetCh    chan GetValueReq
@@ -22,7 +21,6 @@ func NewCore(logger *zap.SugaredLogger) *Core {
 		log:                 logger,
 		data:                make(map[string]interface{}),
 		sortedKeyExpiration: []ExpirationData{},
-		mu:                  sync.RWMutex{},
 
 		SetCh:    make(chan SetCacheReq),
 		GetCh:    make(chan GetValueReq),
@@ -34,9 +32,6 @@ func NewCore(logger *zap.SugaredLogger) *Core {
 }
 
 func (c *Core) set(key string, val interface{}, expUnix int64) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
 	c.data[key] = val
 	expData := ExpirationData{Key: key, Exp: expUnix}
 
@@ -46,15 +41,10 @@ func (c *Core) set(key string, val interface{}, expUnix int64) {
 }
 
 func (c *Core) get(key string) interface{} {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
 	return c.data[key]
 }
 
 func (c *Core) delete(key string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
 	delete(c.data, key)
 }
 
@@ -80,25 +70,27 @@ func (c *Core) processTTLQueue() {
 }
 
 func (c *Core) processTTL() {
-	c.mu.Lock()
 	lenSortedExpKey := len(c.sortedKeyExpiration)
 	if lenSortedExpKey == 0 {
-		c.mu.Unlock()
 		time.Sleep(time.Second)
 		return
 	}
 	firstKeyExpiration := c.sortedKeyExpiration[lenSortedExpKey-1]
 	if firstKeyExpiration.Exp > time.Now().Unix() {
-		c.mu.Unlock()
 		nextTTLTime := time.Unix(firstKeyExpiration.Exp, 0)
 		time.Sleep(nextTTLTime.Sub(time.Now()))
 		return
 	}
 
 	c.sortedKeyExpiration = c.sortedKeyExpiration[:lenSortedExpKey-1]
-	delete(c.data, firstKeyExpiration.Key)
+
+	c.DeleteCh <- DeleteValueReq{
+		Ctx:    context.Background(),
+		Key:    firstKeyExpiration.Key,
+		DoneCh: make(chan struct{}, 1),
+	}
+
 	c.log.Infow("expired", "key", firstKeyExpiration.Key, "time", time.Now())
-	c.mu.Unlock()
 }
 
 func findExpDataInsertingIndex(slice []ExpirationData, data ExpirationData) int {
